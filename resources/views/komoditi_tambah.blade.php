f3477b419c01d47d82a694bf18c604896da10984
viosagara347 <viosagara347@gmail.com>
2020-04-16 22:42:51 +0700

    Update komoditi_tambah.blade.php

---
+++ resources/views/komoditi_tambah.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
+        <title>Tambah Komoditi</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="card mt-5">
+                <div class="card-header text-center">
+                    CRUD Data Komoditi - <strong>TAMBAH DATA</strong></a>
+                </div>
+                <div class="card-body">
+                    <a href="/komoditi" class="btn btn-primary">Kembali</a>
+                    <br/>
+                    <br/>
+                    
+                    <form method="post" action="/komoditi/store">
+
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <label>Nama</label>
+                            <input type="text" name="nama" class="form-control" placeholder="Nama komoditi ..">
+
+                            @if($errors->has('nama'))
+                                <div class="text-danger">
+                                    {{ $errors->first('nama')}}
+                                </div>
+                            @endif
+
+                        </div>
+
+                        <div class="form-group">
+                            <label>Alamat</label>
+                            <textarea name="alamat" class="form-control" placeholder="Alamat komoditi .."></textarea>
+
+                             @if($errors->has('alamat'))
+                                <div class="text-danger">
+                                    {{ $errors->first('alamat')}}
+                                </div>
+                            @endif
+
+                        </div>
+
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-success" value="Simpan">
+                        </div>
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
